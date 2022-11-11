e37f781a5fc051eb7574cc11e35ce65310dceef8
flestnia <cendradewangga@gmail.com>
2022-11-11 17:22:12 +0700

    add dotenv

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const io = require("socket.io");
 const apiServer = require("./api.js");
+require("dotenv").config();
 
 const httpServer = http.createServer(apiServer);
 const socketServer = io(httpServer, {
@@ -14,7 +15,7 @@ const sockets = require("./sockets");
 
 const PORT = 3000;
 
-httpServer.listen(PORT);
+httpServer.listen(process.env.port || PORT);
 console.log("listening to port: ", PORT);
 
 sockets.listen(socketServer);
